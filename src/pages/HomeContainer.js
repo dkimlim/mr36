4224cea16a64b71efc017b79eb4fe5afdfaadea0
diane1234 <dianekimlim@gmail.com>
2020-05-03 18:28:17 -0400

    add node version

--- src/pages/HomeContainer.js
+++ src/pages/HomeContainer.js
@@ -4,7 +4,7 @@ import { Redirect } from 'react-router-dom';
 function Home({ token, setToken }) {
     return (
         <div className="Home">
-        <iframe width="100%" height="640"  frameborder="0" allowfullscreen="true" allow="vr,gyroscope,accelerometer,fullscreen" scrolling="no" src="https://kuula.co/share/7JKxD?fs=1&vr=0&sd=1&thumbs=1&chromeless=0&logo=0&logosize=80"></iframe>
+        <iframe width="100%" height="640" scrolling="no" src="https://kuula.co/share/7XLbv?fs=1&vr=0&sd=1&thumbs=1&chromeless=0&logo=0&logosize=80"></iframe>
         </div>
     );
 }
